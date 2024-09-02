a2a527546e1cf2cd2bc519f61a9abb71da2ee5e7
severity1 <johnreilly.pospos@gmail.com>
2024-09-02 18:07:18 +1200

    test

--- dist/index.js
+++ dist/index.js
@@ -105,8 +105,6 @@ class BedrockAgentRuntimeWrapper {
                 } : {})
             };
     
-            core.info(`Agent invocation params: ${JSON.stringify(commandParams)}`);
-    
             // Create and send the command
             const command = new InvokeAgentCommand(commandParams);
             const response = await this.runtimeClient.send(command);
