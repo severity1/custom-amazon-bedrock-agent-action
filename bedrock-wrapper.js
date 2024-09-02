16a3bcc86f088b9199c87257bceb8fd7470cc508
severity1 <johnreilly.pospos@gmail.com>
2024-09-03 10:31:18 +1200

    test

--- bedrock-wrapper.js
+++ bedrock-wrapper.js
@@ -15,7 +15,6 @@ class BedrockAgentRuntimeWrapper {
             agentAliasId,
             sessionId,
             inputText: prompt,
-            enableTrace: true,
             memoryId
         });
 
