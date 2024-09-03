41bb8e62a1bd97541bc908185db2e3079bfcd23f
severity1 <johnreilly.pospos@gmail.com>
2024-09-03 16:54:03 +1200

    test

--- dist/index.js
+++ dist/index.js
@@ -57744,7 +57744,7 @@ async function main() {
 
         // const prompt = `${codePrompt}\n${diffsPrompt}\n${actionPrompt}\nFormat your response using Markdown, including appropriate headers and code blocks where relevant.`;
         // const prompt = `${codePrompt}\n${diffsPrompt}\n${actionPrompt}\nFormat your response using Markdown, including appropriate headers and code blocks where relevant.`;
-        const prompt = `${diffsPrompt}\n${actionPrompt}\nFormat your response using Markdown, including appropriate headers and code blocks where relevant.`;
+        const prompt = `${codePrompt}\n${actionPrompt}`;
 
         // Validate that the prompt is a valid string
         if (typeof prompt !== 'string' || prompt.trim() === '') {
